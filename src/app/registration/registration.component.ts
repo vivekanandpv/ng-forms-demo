8387a9465debe872aa66875981b349fd4d6c05e8
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-10 12:28:19 +0530

    validation messages

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
   form: FormGroup;
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
@@ -19,15 +25,23 @@ export class RegistrationComponent implements OnInit {
         [
           Validators.required,
           Validators.minLength(10),
-          Validators.maxLength(11)
-        ]
+          Validators.maxLength(11),
+        ],
       ],
       dateOfBirth: [''],
-      password: ['', [Validators.required, Validators.pattern('[0-9]*')]]
+      password: ['', [Validators.required, Validators.pattern('[0-9]*')]],
     });
   }
 
+  public get password(): FormControl {
+    return this.form.controls['password'] as FormControl;
+  }
+
   onSubmit() {
-    console.log('Form Posted', this.form.value);
+    if (this.form.valid) {
+      console.log('Form Posted', this.form.value);
+    } else {
+      console.log('Not valid');
+    }
   }
 }
